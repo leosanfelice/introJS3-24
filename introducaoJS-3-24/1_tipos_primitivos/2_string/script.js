d26811d50156ad6fbf2160f2566aecfcf4907ba1
Leonardo munhos sanfelice <126088427+leosanfelice@users.noreply.github.com>
2024-03-06 21:07:38 -0300

    Add files via upload

---
+++ introducaoJS-3-24/1_tipos_primitivos/2_string/script.js
@@ -0,0 +1,14 @@
+const texto1 = "kaique cupin";
+const texto2 = 'gabziin';
+const texto3 = ' foi no "Banheiro"';
+
+console.log(texto1);
+console.log(texto2);
+
+console.log(texto2 + texto3);
+
+const cifrao = '\u0024';
+const aMaiusculo = '\u0041';
+
+console.log(cifrao);
+console.log(aMaiusculo);
