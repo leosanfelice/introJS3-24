ca11d4c9cffda9ea0b41a3a47c0b5db76e477a48
Leonardo munhos sanfelice <126088427+leosanfelice@users.noreply.github.com>
2024-03-15 22:10:33 -0300

    Add files via upload

---
+++ 1_tipos_primitivos/3_boolean/script.js
@@ -0,0 +1,21 @@
+const text1 = "Olá mundo";
+const text2 = 'Olá mundo';
+const senha = "senhasupersegura";
+const StringDeNumeros = "3642";
+
+// concatenação (+)
+
+const citacao = "Meu nome é";
+const meuNome = " Leo" ;
+
+console.log(citacao + meuNome);
+
+//comparaçao
+
+console.log(text1 === text2);
+
+const n1 = 1;
+
+const n2 = 2;
+
+console.log(n1 === n2)
