5f64785e94b02d90e1ca6ab7c97671676659de4c
Leonardo munhos sanfelice <126088427+leosanfelice@users.noreply.github.com>
2024-03-25 20:19:21 -0300

    Add files via upload

---
+++ introJS3-24-main/introJS3-24-main/1_tipos_primitivos/1_number/script.js
@@ -0,0 +1,16 @@
+//tipo number
+
+const meuNumero = 12;
+
+const telefone = 44998268318;
+
+const idade = 16;
+
+console.log(meuNumero);
+console.log(telefone);
+console.log(idade);
+
+
+const soma = meuNumero + idade
+
+console.log(soma)
---
+++ introJS3-24-main/introJS3-24-main/2_declarando_variaveis/5_null_undefined/script.js
@@ -0,0 +1,30 @@
+var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+console.log(area);
+var area;
+
+// 
+
+let forma = 'retangulo';
+let altura2 = 5;
+let comprimento2 = 7;
+
+if (forma === 'retangulo') {
+    area = altura2 * comprimento2;
+}
+else {
+    area = altura2 * comprimento2 / 2;
+}
+console.log(area);
+
+//
+
+const forma3 = 'quadrado';
+const altura3 = 5;
+const comprimento3 = 7;
+
+const area3 = altura3 * comprimento3;
+
+console.log(area3);
